ef90156b1c24b9efcefac13f5ed416344853b343
Awnish Singh <dm.singh723dm@gmail.com>
2021-03-14 15:57:08 +0530

    First commit

---
+++ djrestmongo/urls.py
@@ -0,0 +1,25 @@
+"""djrestmongo URL Configuration
+
+The `urlpatterns` list routes URLs to  For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from tutorials.views import tutorial_detail,tutorial_list,tutorial_list_published
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/tutorials', tutorial_list),
+    path('api/tutorials/<int:pk>', tutorial_detail),
+    path('api/tutorials/published', tutorial_list_published)
+]
